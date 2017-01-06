9027c88d60f6538bdc89e059d76331c9cf5649b2
Stone Westray <stwestra@umass.edu>
2017-01-06 14:45:52 -0500

    new proj

---
+++ public/core.js
@@ -0,0 +1,109 @@
+var app = angular.module('Feeder', []);
+
+app.controller('recipeController', function($scope, $http){
+	$scope.formData = {};
+
+    // when landing on the page, get all recipes and show them
+    $http.get('/api/recipes')
+        .success(function(data) {
+            $scope.recipes = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the data to the node API
+    $scope.createRecipe = function() {
+        $http.post('/api/recipes', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.recipes = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a recipe after checking it
+    $scope.deleteRecipe = function(id) {
+        $http.delete('/api/recipes/' + id)
+            .success(function(data) {
+                $scope.recipes = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+});
+
+app.controller('CollapseController', function($scope){
+	var collapsed = true;
+
+	$scope.collapse = function(product){
+		if(collapsed){
+			$scope.collapseBtn = 'glyphicon glyphicon-chevron-up'
+			collapsed = false;
+		}
+		else{
+			$scope.collapseBtn = 'glyphicon glyphicon-chevron-down'
+			collapsed = true;
+		}
+	}
+});
+
+app.controller('YumController', function($scope){
+	var isYummed = false;
+	var isYucked = false;
+	$scope.yum = function(recipe) {
+		if(isYummed){
+			isYummed = false;
+			$scope.yumButton = 'yum';
+			recipe.score -= 1
+    };
+		}
+		else if(isYucked){
+			isYucked = false;
+			isYummed = true;
+			$scope.yumButton = 'YUM';
+			$scope.yuckButton = 'yuck';
+			recipe.score += 2;
+		}
+		else{
+			isYummed = true;
+			$scope.yumButton = 'YUM'
+			recipe.score += 1;
+		}
+	};
+	$scope.yuck = function (recipe) {
+		if(isYucked){
+			isYucked = false;
+			$scope.yuckButton = 'yuck'
+			recipe.score += 1;
+		}
+		else if(isYummed){
+			isYummed = false;
+			isYucked = true;
+			$scope.yuckButton = 'YUCK';
+			$scope.yumButton = 'yum';
+			recipe.score -= 2;
+		}
+		else{
+			isYucked = true;
+			$scope.yuckButton = 'YUCK'
+			recipe.score -= 1;
+		}
+	};
+});
+
+app.controller('TabController', function($scope){
+    $scope.tab = 1;
+    $scope.setTab = function(newValue){
+    $scope.tab = newValue;
+   	};
+   	$scope.isSet = function(tabName){
+   		return $scope.tab === tabName;
+   	};
+});
---
+++ server.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://stone:westray@ds037215.mlab.com:37215/feeder');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended' : 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+//model
+var Recipe = mongoose.model('Recipe', {
+	title: String,
+	description: String,
+	score: { type: Number, default: 0 }
+});
+
+    // API
+    // get all recipes
+    app.get('/api/recipes', function(req, res) {
+
+        // use mongoose to get all recipes in the database
+        Recipe.find(function(err, recipes) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(recipes); // return all recipes in JSON format
+        });
+    });
+
+    // create recipe and send back all recipes after creation
+    app.post('/api/recipes', function(req, res) {
+
+        // create a recipe, information comes from AJAX request from Angular
+        Recipe.create({
+            title : req.body.title,
+            description: req.body.description,
+            score: req.body.score
+        }, function(err, recipe) {
+            if (err)
+                res.send(err);
+
+            // get and return all the recipes after you create another
+            Recipe.find(function(err, recipes) {
+                if (err)
+                    res.send(err)
+                res.json(recipes);
+            });
+        });
+
+    });
+
+    //update a recipe with new score
+    app.put('/api/recipes/:recipe_id', function(req, res){
+    	Recipe.findByIdAndUpdate(req.params.recipe_id, function(err, recipe){
+    		if(err)
+    			res.send(err);
+
+    		recipe.score = req.body.score;
+
+    		recipe.save(function(err){
+    			if(err)
+    				res.send(err);
+
+    			res.json({ message: 'recipe updated!'});
+    		});
+    	});
+    });
+
+    // delete a recipe
+    app.delete('/api/recipes/:recipe_id', function(req, res) {
+        Recipe.remove({
+            _id : req.params.recipe_id
+        }, function(err, recipe) {
+            if (err)
+                res.send(err);
+
+            // get and return all the recipes after you create another
+            Recipe.find(function(err, recipes) {
+                if (err)
+                    res.send(err)
+                res.json(recipes);
+            });
+        });
+    });
+
+    app.get('*', function(req, res){
+    	res.sendfile('./public/index.html');
+    });
+
+
+app.listen(8000);
+console.log('app is listening on port 8000');
